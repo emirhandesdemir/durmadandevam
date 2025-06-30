6f6316d333038209964a57979b7704497d0d9304
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-06-30 14:07:32 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,69 +1,6 @@
 
 import type {NextConfig} from 'next';
 
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  workboxOptions: {
-    importScripts: ['/firebase-messaging-sw.js'],
-    runtimeCaching: [
-        {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'google-fonts-cache',
-                expiration: {
-                    maxEntries: 10,
-                    maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-                },
-                cacheableResponse: {
-                    statuses: [0, 200],
-                },
-            },
-        },
-        {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'gstatic-fonts-cache',
-                expiration: {
-                    maxEntries: 10,
-                    maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-                },
-                cacheableResponse: {
-                    statuses: [0, 200],
-                },
-            },
-        },
-        {
-            urlPattern: /\.(?:png|gif|jpg|jpeg|svg|webp)$/i,
-            handler: 'StaleWhileRevalidate',
-            options: {
-                cacheName: 'images-cache',
-                expiration: {
-                    maxEntries: 60,
-                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-                },
-            },
-        },
-        {
-            urlPattern: ({ url, sameOrigin }: {url: URL, sameOrigin: boolean}) => sameOrigin,
-            handler: 'NetworkFirst',
-            options: {
-                cacheName: 'pages-cache',
-                expiration: {
-                    maxEntries: 60,
-                    maxAgeSeconds: 24 * 60 * 60, // 24 hours
-                },
-                networkTimeoutSeconds: 3,
-            },
-        },
-    ]
-  },
-});
-
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -96,4 +33,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
