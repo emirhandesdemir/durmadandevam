8615dc79df816324c3f9b21cea44db03e6a4b779
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-07-06 10:49:02 +0000

    Pwabuilder ile sistemi  google play storede yayina hazir hale getiriyoru

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   swSrc: 'src/sw.ts', // Use custom service worker
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
 });
 
 /** @type {import('next').NextConfig} */
