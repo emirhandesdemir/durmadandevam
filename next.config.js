e676ed1712349da748c7dde3509d98c63f61074e
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-07-08 11:51:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,4 +1,42 @@
-// This file is obsolete because next.config.ts takes precedence.
-// The configuration has been moved to next.config.ts.
-const config = {};
-module.exports = config;
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  importScripts: ['/firebase-messaging-sw.js'],
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
