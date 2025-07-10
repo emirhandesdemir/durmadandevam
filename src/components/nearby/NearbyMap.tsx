a9dd4c690fde6f501983a2036b372cfa670a502b
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-07-10 08:00:54 +0000

    Harita Snapchat gibi olsun teması pastel renkler olsun çok gerçekçi olma

--- src/components/nearby/NearbyMap.tsx
+++ src/components/nearby/NearbyMap.tsx
@@ -42,8 +42,8 @@ export default function NearbyMap({ currentUserPosition, users }: NearbyMapProps
   return (
     <MapContainer center={currentUserPosition} zoom={14} scrollWheelZoom={true} className="w-full h-full z-0">
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
       />
       {/* Current User Marker */}
       <Marker position={currentUserPosition} icon={defaultIcon}>
