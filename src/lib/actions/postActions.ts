68136d445e088cd7f979d2da5caf4b2179c8aab4
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-07-16 13:38:07 +0000

    Bu hatayı alıyorum resim paylaşırken birde ai ile düzenleme yaparken hat

--- src/lib/actions/postActions.ts
+++ src/lib/actions/postActions.ts
@@ -16,7 +16,7 @@ import {
     collection,
     writeBatch
 } from "firebase/firestore";
-import { ref, deleteObject, uploadString, getDownloadURL } from "firebase/storage";
+import { ref, deleteObject, uploadString, getDownloadURL, uploadBytes } from "firebase/storage";
 import { revalidatePath } from "next/cache";
 import { createNotification } from "./notificationActions";
 import { findUserByUsername } from "./userActions";
