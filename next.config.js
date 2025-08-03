266e8f37091c9ab4e673592abba88cad591e4a7d
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-08-03 05:48:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -42,23 +42,6 @@ const nextConfig = {
       }
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin-allow-popups',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
