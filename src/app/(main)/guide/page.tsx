25ff3c12183837dbcd342f15e36778c31931c83a
Emir ÖzArslan <emirhandesdemir@gmail.com>
2025-08-03 21:02:12 +0000

    11:55:38 PM: build-image version: c440bb785c036c7461456dcbf56442db2c6157

--- src/app/(main)/guide/page.tsx
+++
@@ -1,9 +0,0 @@
-// Bu sayfa artık sadece NewPostForm bileşenini render ediyor.
-// Tüm layout ve stil mantığı, daha iyi bir yapı için NewPostForm'un içine taşındı.
-import NewPostForm from "@/components/posts/NewPostForm";
-
-export default function CreatePostPage() {
-  return (
-    <NewPostForm />
-  );
-}
